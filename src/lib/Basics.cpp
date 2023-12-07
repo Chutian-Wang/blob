9dd7e6625133f3fd5593cb6810110a355f64be17
Evania Cheng <evania@ucsb.edu>
2023-12-06 19:24:59 -0800

    Basics completed

---
+++ src/lib/Basics.cpp
@@ -0,0 +1,35 @@
+#include "Basics.h"
+#include "Color.h"
+
+#include <cmath>
+
+void Basics::DrawCircle(float x, float y, float radius, const Color& color, int segments) {
+    glColor3ub(color.r, color.g, color.b);
+    glBegin(GL_TRIANGLE_FAN);
+
+    for (int i = 0; i <= segments; ++i) {
+        float theta = 2.0f * 3.141592653f * float(i) / float(segments);
+        float dx = radius * cosf(theta);
+        glVertex2f(x + dx, y + dy);
+    }
+    glEnd();
+}
+    
+void Basics::DrawLine(float x1, float y1, float x2, float y2, const Color& color) {
+    glColor3ub(color.r, color.g, color.b);
+    glBegin(GL_LINES);
+    glVertex2f(x1, y1);
+    glVertex2f(x2, y2);
+    glEnd();
+}
+
+void Basics::DrawText(float x, float y, const Color& color, float a, float scale) {
+    glColor3ub(color.r, color.g, color.b);
+    glRasterPos2f(x, y);
+
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, 'S');
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, 't');
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, 'a');
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, 'r');
+    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, 't');
+}
--- src/lib/Basics.h
+++ src/lib/Basics.h
@@ -1,6 +1,9 @@
 #ifndef BASICS_H
 #define BASICS_H
 
+#include <GL/gl.h>
+#include <GL/glut.h>
+
 namespace Basics {
     void DrawCircle(float x, float y, float radius, const Color& color, int segments = 16);
     void DrawLine(float x1, float y1, float x2, float y2, const Color& color);
